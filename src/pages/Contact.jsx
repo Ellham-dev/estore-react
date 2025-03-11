5c07859a608cea00da53bc5b9e0c0d99ce5001d4
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-11 23:58:52 +0600

    contact update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,6 +8,14 @@ const Contact = () => {
         <div className="flex items-center">
           <h4 className="font-semibold text-2xl leading-8 text-center">Contact Us</h4>
         </div>
+        <div className="">
+          <div className="">
+            <span>Address</span>
+            <p>234 Hai Trieu, Ho Chi Minh City, 
+            Viet Nam</p>
+          </div>
+          
+        </div>
       </div>
     </section>
     </>
