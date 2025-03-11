db23001fb4ef213f9d1b403131d91b6fcc06a810
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-11 23:54:30 +0600

    contact page start

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,15 @@
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <section>
+      <div className="max-w-[1120px] m-auto">
+        <div className="flex items-center">
+          <h4 className="font-semibold text-2xl leading-8 text-center">Contact Us</h4>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
