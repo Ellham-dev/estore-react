8ca682dad7b04ed51b8f48f2df3b3811844089a6
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-16 02:02:22 +0600

    chart page customize

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -23,24 +23,32 @@ const Chart = () => {
                                 </div>
                                 <div className="">
                                     <p className='text-sm leading-4 font-medium text-[#545454] mb-2'>Your bonus card number</p>
-                                    <input type="text" placeholder='Enter Card Number' className='w-[408px] p-4 text-sm leading-6 border outline-none border-[#B5B5B5] rounded-lg mb-6' />
+                                    <div className="w-[408px] h-16 flex items-center justify-between border outline-none border-[#B5B5B5] rounded-lg px-4   mb-6">
+                                        <input type="text" placeholder='Enter Card Number' className=' text-sm leading-6 outline-none w-[299px]' />
+                                        <button className='text-[12px] leading-4 font-medium p-2 border border-black rounded-lg w-[76px] cursor-pointer'>Apply</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flex items-center justify-between mb-5">
-                                <h6 className='text-[16px] leading-6 font-medium'>Subtotal</h6>
-                                <p className='text-[16px] leading-8 font-medium '>$2347</p>
+                            <div className="mb-12">
+                                <div className="flex items-center justify-between mb-5">
+                                    <h6 className='text-[16px] leading-6 font-medium'>Subtotal</h6>
+                                    <p className='text-[16px] leading-8 font-medium '>$2347</p>
+                                </div>
+                                <div className="flex items-center justify-between mb-2">
+                                    <h6 className='text-[16px] leading-8 text-[#545454] '>Estimated Tax</h6>
+                                    <p className='text-[16px] leading-8 font-medium '>$50</p>
+                                </div>
+                                <div className="flex items-center justify-between mb-5">
+                                    <h6 className='text-[16px] leading-8 text-[#545454] '>Estimated shipping & Handling</h6>
+                                    <p className='text-[16px] leading-8 font-medium '>$29</p>
+                                </div>
+                                <div className="flex items-center justify-between ">
+                                    <h6 className='text-[16px] leading-6 font-medium '>Total</h6>
+                                    <p className='text-[16px] leading-8 font-medium '>$2426</p>
+                                </div>
                             </div>
-                            <div className="flex items-center justify-between mb-2">
-                                <h6 className='text-[16px] leading-8 text-[#545454] '>Estimated Tax</h6>
-                                <p className='text-[16px] leading-8 font-medium '>$50</p>
-                            </div>
-                            <div className="flex items-center justify-between mb-5">
-                                <h6 className='text-[16px] leading-8 text-[#545454] '>Estimated shipping & Handling</h6>
-                                <p className='text-[16px] leading-8 font-medium '>$29</p>
-                            </div>
-                            <div className="flex items-center justify-between ">
-                                <h6 className='text-[16px] leading-6 font-medium '>Total</h6>
-                                <p className='text-[16px] leading-8 font-medium '>$2426</p>
+                            <div className="">
+                                <button className='bg-black text-white py-4 w-full rounded-xl text-[16px] leading-6 font-medium cursor-pointer'>Checkout</button>
                             </div>
                         </div>
                     </div>
