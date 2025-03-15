b08811e139af49651c9e2a9c38436a3a4360ca36
Ellham Khan <ellhamkhan444@gmail.com>
2025-03-15 23:16:32 +0600

    single product chart complete

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import ProductChart from './ProductChart'
 
 const Chart = () => {
-  return (
-    <div>Chart</div>
-  )
+    return (
+        <>
+            <section>
+                <div className="max-w-[1120px] m-auto">
+                    <div className="py-[112px]">
+                        <div className="">
+                            <h5 className='text-2xl leading-6 font-semibold mb-10'>Shopping Cart</h5>
+                            {/* single_product_chart */}
+                            <ProductChart/>
+                            <ProductChart/>
+                            <ProductChart/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Chart
