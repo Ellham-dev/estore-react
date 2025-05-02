882ae1b260f78b3d3626900c315ff93a07e518e5
Ellham Khan <ellhamkhan444@gmail.com>
2025-05-02 22:23:49 +0600

    new product add

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -30,7 +30,7 @@ const Products = () => {
               <Link to={"product"}><Product Src={Headphone1} text={"AirPods Max Silver Starlight Aluminium"} Price={564} /></Link>
               <Link to={"product"}><Product Src={Watch2} text={"Samsung Galaxy Watch6 Classic 47mm Black"} Price={364} /></Link>
               <Link to={"product"}><Product Src={GalaxyZ} text={"Galaxy Z Fold5 Unlocked | 256GB | Phantom Black"} Price={1790} /></Link>
-              <Link to={"product"}><Product Src={GalaxyBuds1} text={"Galaxy Buds FE Graphite"} Price={99} /></Link>
+              <Link to={"product"}><Product Src={GalaxyBuds1} text={"Galaxy Buds FE Graphite 47mm Black"} Price={99} /></Link>
               <Link to={"product"}><Product Src={Iped1} text={'Apple iPad 9 10.2" 64GB Wi-Fi Silver (MK2L3) 2021'} Price={399} /></Link>
             </div>
           </div>
