218bf6e0d19e1f68101cbec0cfced8f300d3eb05
Ellham Khan <ellhamkhan444@gmail.com>
2025-05-02 22:23:26 +0600

    Product add

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router"
 import Product from "./Product"
-
+import Watch2 from '/Watch2.png'
+import GalaxyZ from '/GalaxyZ.png'
+import GalaxyBuds1 from '/GalaxyBuds1.png'
+import Iped1 from '/Iped1.png'
 
 const Discount = () => {
   return (
@@ -13,10 +16,10 @@ const Discount = () => {
             </div>
             <div className="flex gap-x-4">
               
-              <Link to={"product"}><Product/></Link>
-              <Link to={"product"}><Product/></Link>
-              <Link to={"product"}><Product/></Link>
-              <Link to={"product"}><Product/></Link>
+              <Link to={"product"}><Product Src={GalaxyZ} text={"Galaxy Z Fold5 Unlocked | 256GB | Phantom Black"} Price={1790}/></Link>
+              <Link to={"product"}><Product Src={GalaxyBuds1} text={"Galaxy Buds FE Graphite 47mm Black"} Price={99}/></Link>
+              <Link to={"product"}><Product Src={Watch2} text={"Samsung Galaxy Watch6 Classic 47mm Black"} Price={364}/></Link>
+              <Link to={"product"}><Product Src={Iped1} text={'Apple iPad 9 10.2" 64GB Wi-Fi Silver (MK2L3) 2021'} Price={399}/></Link>
             </div>
           </div>
         </div>
--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import popularProduct from '/Gelaxxy.png'
 
-const PopularProduct = ({className}) => {
+
+const PopularProduct = ({GridSrc,GridHeadText,GridPeraText}) => {
     return (
         <>
-            <div className={`w-1/4 flex flex-col items-center px-8 ${className}`}>
+            <div className={"w-1/4 flex flex-col items-center px-8"}>
                 {/* img_part */}
                 <div className="">
-                    <img src={popularProduct} alt="" />
+                    <img src={GridSrc} alt="" />
                 </div>
                 {/* title_part */}
                 <div className="">
-                    <h4 className={`text-[33px] font-light leading-12 mb-4 text-[#000000] ${className}`}>Popular Products</h4>
-                    <p className='text-sm font-medium leading-6 mb-4 w-[296px] text-[#909090]'>iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use.</p>
+                    <h4 className={"text-[33px] font-light leading-12 mb-4 text-[#000000]"}>{GridHeadText}</h4>
+                    <p className='text-sm font-medium leading-6 mb-4 w-[296px] text-[#909090]'>{GridPeraText}</p>
                     <div className="text-left">
                         <button className="Text-[16px] font-medium leading-6 border px-14 py-4 rounded-md cursor-pointer mb-14">Buy Now</button>
+                        {/* GridSrc={popularProduct} GridHeadText={"Samsung Galaxy"} GridPeraText={"iPad combines a magnificent 10.2-inch Retina display, incredible performance, multitasking and ease of use. */}
                     </div>
                 </div>
             </div>
